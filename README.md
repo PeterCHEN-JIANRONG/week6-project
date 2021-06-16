cbd5153be5a15fb9e1bc0227e4262aa03846400b
peter.chen1024 <u0052017@gmail.com>
2021-06-16 15:41:57 +0800

    前台router初始架構, 新增前台導覽列

--- README.md
+++ README.md
@@ -1,24 +1,39 @@
 # week6-project
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+=====
+
+### 目錄結構
+
+#### assets
+
+- components 元件的 scss
+- helpers 自訂變數 scss
+- all.scss 做 scss 總管
---
+++ src/views/Front.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">導覽列</a>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link active" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/products">Products</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">About</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  前台
+  <router-view />
+</template>
