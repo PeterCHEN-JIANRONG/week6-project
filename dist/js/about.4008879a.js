bacdd6a2e504d8b8582041d84e205fcc351b4c2d
peter.chen1024 <u0052017@gmail.com>
2021-06-18 16:52:02 +0800

    add dist folder

---
+++ dist/js/about.4008879a.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(e,c,n){"use strict";n.r(c);var t=n("7a23"),o={class:"container about"},a=Object(t["createVNode"])("h1",null,"關於我們",-1);function r(e,c){return Object(t["openBlock"])(),Object(t["createBlock"])("div",o,[a])}const u={};u.render=r;c["default"]=u}}]);
+//# sourceMappingURL=about.4008879a.js.map
