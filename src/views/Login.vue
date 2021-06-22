038804b4fa45f1a140fd41e18e914fd630bf5ab7
peter.chen1024 <u0052017@gmail.com>
2021-06-22 15:57:13 +0800

    修改後台登入驗證

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,37 +1,43 @@
 <template>
   <div class="container mt-5">
-    <form class="row justify-content-center" @submit.prevent="signIn">
-      <div class="col-md-6">
-        <h1 class="h3 mb-3 font-weight-normal">後台登入</h1>
-        <div class="mb-2">
-          <label for="inputEmail" class="sr-only">Email address</label>
-          <input
+    <!-- 表單送出 -->
+    <div class="my-5 row justify-content-center">
+      <Form ref="form" class="col-md-6" v-slot="{ errors }" @submit="signIn">
+        <h1 class="h3 mb-3 font-weight-normal">請先登入</h1>
+        <div class="mb-3">
+          <label for="email" class="form-label">Email</label>
+          <Field
+            id="email"
+            name="email"
             type="email"
-            id="inputEmail"
+            placeholder="請輸入 Email"
             class="form-control"
-            placeholder="Email address"
+            :class="{ 'is-invalid': errors['email'] }"
+            rules="email|required"
             v-model="user.username"
-            required
-            autofocus
-          />
-        </div>
-        <div class="mb-2">
-          <label for="inputPassword" class="sr-only">Password</label>
-          <input
-            type="password"
-            id="inputPassword"
-            class="form-control"
-            v-model="user.password"
-            placeholder="Password"
-            required
-          />
+          ></Field>
+          <ErrorMessage name="email" class="invalid-feedback"></ErrorMessage>
         </div>
 
-        <div class="text-end mt-4">
-          <button class="btn btn-lg btn-primary btn-block" type="submit">登入</button>
+        <div class="mb-3">
+          <label for="password" class="form-label">密碼</label>
+          <Field
+            id="password"
+            name="密碼"
+            type="text"
+            placeholder="請輸入密碼"
+            class="form-control"
+            :class="{ 'is-invalid': errors['密碼'] }"
+            rules="required"
+            v-model="user.password"
+          ></Field>
+          <ErrorMessage name="密碼" class="invalid-feedback"></ErrorMessage>
         </div>
-      </div>
-    </form>
+        <div class="text-end mt-4">
+          <button type="submit" class="btn btn-lg btn-primary btn-block">登入</button>
+        </div>
+      </Form>
+    </div>
   </div>
 </template>
 
